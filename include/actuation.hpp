85ea27ed1549ac9bc4a95b5fab643a517848309b
张甲晨 <jiachen@MBP-JZhang.local>
2018-02-15 11:39:08 -0500

    move controller to a class

--- include/actuation.hpp
+++ include/actuation.hpp
@@ -23,16 +23,13 @@ class Coil_System {
         Coil_System ( void );
         void set_uniform_field_volt ( float data[3] );
         void set_z_field_volt (float data);
-        void set_gradient_field_volt ( float data[6] );
         void output_signal ( void );
         void set_angle (float data);
         void rotate_to_new_angle ( void );
         void stop_output (void);
         void add_gradient_output (void);        // add gradient along the moving direction
     private:
-        float outputV[6];       // output voltage for +x, -x, +y, -y, +z, -z
         float uniformV[3];
-        float gradientV[6];
         float angle, angleOld;            // field angle in X-Y plane in degrees
         int fGradient;                  // whether or not add gradient to output
 };
